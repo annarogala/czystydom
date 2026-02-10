27348fb28f990b75430ad82fd747214c1c546355
Anna Rogala <anna.rogala@stxnext.pl>
2026-02-10 11:03:44 +0100

    correct cors header

--- api/todos/[id].js
+++ api/todos/[id].js
@@ -2,9 +2,9 @@ import { getTodos, setTodos } from '../lib/db.js';
 
 export default async function handler(req, res) {
   // Enable CORS
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
